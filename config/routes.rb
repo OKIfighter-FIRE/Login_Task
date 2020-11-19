2f3b6db9dc2b3a1887703f39a189d821ddd16bb5
kinjo_s <hahaha.kinjo0330@gmail.com>
2020-11-19 19:50:46 +0900

    ログイン機能実装

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get "users/show"
+  # get "items/index"
+  # get "items/new"
+  # get "items/create"
+  root to: "items#index"
+  devise_for :users
+  resources :items
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :users, only: :show
+end
